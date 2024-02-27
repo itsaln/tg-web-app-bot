2e2ecf00eb552e652f2881546d670845ca9a896a
itsaln <itsaln23@gmail.com>
2024-02-27 12:31:45 +0500

    web-data

--- index.js
+++ index.js
@@ -53,8 +53,6 @@ bot.on('message', async (msg) => {
 app.post('/web-data',  async (req, res) => {
 	const { queryId, products = [], totalPrice } = req.body
 	
-	console.log('queryId:--', queryId)
-	
 	try {
 		await bot.answerWebAppQuery(queryId, {
 			type: 'article',
